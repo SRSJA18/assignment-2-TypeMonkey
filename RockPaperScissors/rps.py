8a8581a2bcc4e3e593bc54523fb6986024db72c0
Stephen Cerruti <scerruti@e3civichigh.com>
2018-06-26 09:54:52 -0700

    Updated for Python 3 and to clean up rps.py

--- RockPaperScissors/rps.py
+++ RockPaperScissors/rps.py
@@ -1,15 +1,25 @@
-# Please reference the included PDF for complete instructions.
-#
-# You can create your table (from the instructions) using a doc or spreadsheet.
-#
-# Advanced students should attempt to implement
-# Rock, Paper, Scissors, Lizard, Spock using 3 or 4 players instead (This is much harder.)
-# 1. Ask player 1 for their move.
+"""Rock Paper Scissors"""
+'''
+Please reference the included PDF for complete instructions.
 
-# 2. Check if player 1's move is valid.
+You can create your table (from the instructions) using a doc or spreadsheet.
 
-# 3. Ask player 2 for their move.
+Text comparisons are case sensitive. We will learn how to deal with this later.
 
-# 4. Check if player 1's move is valid.
+Advanced students should attempt to implement
+Rock, Paper, Scissors, Lizard, Spock using 3 or 4 players instead (This is much harder.)
+'''
 
-# 5. Print out the winner or 'tie'
+# TODO 1. Ask player 1 for their move.
+
+# TODO 2. Ask player 2 for their move.
+
+# TODO 3. Create 9 if statements matching your table, print out the result of each match. (Test)
+
+# TODO 4. Change your first if statement to check if player 1's move equals player 2's move. (Test)
+
+# TODO 5. Change all but the first if statement to elif. (Test)
+
+# TODO 6. Add an else statement at the end to check for an invalid move. (TEST)
+
+# TODO 7. Nest if statements instead of using and.
